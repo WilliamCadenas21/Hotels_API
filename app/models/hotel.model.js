5d608f8dae3681df651d87bb6769ad654244be7c
William Cadenas <williamcadenas21@gmail.com>
2018-10-12 12:13:04 -0500

    add configdb functionality

--- app/models/hotel.model.js
+++ app/models/hotel.model.js
@@ -1,17 +1,15 @@
 const mongoose = require('mongoose')
 
 const HotelSchema = mongoose.Schema({   
- 	rooms: Number,
- 	hotel_name: String,
- 	address: String,
- 	state: String,
- 	phone: String,
- 	fax: String,
- 	email_id: String,
- 	website: String,
- 	type: String,
-}, {
-    timestamps: true
-});
+ 	rooms: String,
+ 	hotel_name: String, 
+    address : String,
+    state : String,
+    phone : String,
+    fax : String,
+    email_id : String,
+    website : String,
+    type : String
+})
 
 module.exports = mongoose.model('Hotel', HotelSchema)
